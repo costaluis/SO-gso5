e5b8eda5656dfdbeaf67371e16b1664293442e9f
costaluis <luis_oliveira@usp.br>
2020-03-28 18:25:43 -0300

    Função de processos alterada

--- prog2.c
+++ prog2.c
@@ -1,6 +1,14 @@
 //Programa que realiza system calls de gerenciamento de processos.
 //clone(), wait4(), exit_group()
 
+//A syscall clone() cria um novo processo (“filho”) idêntico ao “pai”, com diferenças apenas no PID e no espaço de endereçamento de memória. Age como um wrapper da syscall “fork()”, utilizada explicitamente no código. 
+
+//A função waitpid(), utilizada explicitamente no código, é implementada como uma função wrapper que chama wait4(). Sua funcionalidade é parar a execução até que um processo especificado mude de estado.
+
+//A syscall kill() é utilizada para enviar um sinal qualquer à um processo ou grupo de processos.
+
+//system calls envolvidas(tempo em microssegundos): wait4(634); write(420); execve(130); clone(53); mmap(41); mprotect(39); access(23); kill(22); fstat(19); brk(19); openat(18); munmap(17); close(12); read(7); arch_prctl(5)
+
 //Chamadas das bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,6 +41,8 @@ int main(int argv, char * argc[]){
             printf("Pai: %d ",i);
             printf("\n");
         }
+        //Quando o signal eh 0, o comando nao envia o signal, mas a execucao ocorre, checando a existencia do processo.
+        kill(WUNTRACED,0);
     }
     
     return 0;
