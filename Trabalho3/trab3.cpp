d6b623f554ba5097c75ac641c53990caf0e71b04
costaluis <luis_oliveira@usp.br>
2020-06-18 17:59:36 -0300

    Aparentemente funciona

--- Trabalho3/trab3.cpp
+++ Trabalho3/trab3.cpp
@@ -7,17 +7,18 @@
 #include <stack>
 using namespace std;
 
-#define TAM_MEM_P 2000
+#define TAM_MEM_P 200
 #define TAM_MEM_S 20000
 #define PAG_SIZE 100
 
 class pagina
 {
 private:
+public:
     char dados[PAG_SIZE];
     int process_id;
-public:
-    int end;
+    int R;
+    pagina();
     pagina(int id);
     ~pagina();
 };
@@ -25,13 +26,24 @@ public:
 class processo
 {
 private:
-    int id;
     int tam;
 public:
+    int id;
     vector<pagina*> paginas;
     map<pagina*,pagina*> tabela_paginas;
     processo(int identificador, int tamanho);
     ~processo();
+};
+
+class lista_circular
+{
+private:
+public:
+    vector<pagina*> mem_p;
+    int pont;
+    lista_circular();
+    ~lista_circular();
+    void relogio(processo * proc, pagina * pag, vector<processo*> & processos);
 };
 
 class mem
@@ -39,7 +51,7 @@ class mem
 private:
 public:
     vector<pagina*> paginas_mem;
-    stack<int> prox_livre;
+    int tam;
     mem(int tam);
     ~mem();
 };
@@ -47,6 +59,14 @@ public:
 pagina::pagina(int id)
 {
     process_id = id;
+    R = 0;
+    for(int i=0; i<PAG_SIZE; i++){
+        dados[i] = 'a'+i;
+    }
+}
+
+pagina::pagina()
+{
 }
 
 pagina::~pagina()
@@ -67,23 +87,87 @@ processo::~processo()
 {
 }
 
+lista_circular::lista_circular()
+{
+}
+
+lista_circular::~lista_circular()
+{
+}
+
+void lista_circular::relogio(processo * proc, pagina * pag, vector<processo*> & processos){
+    pagina * aux = new pagina();
+    int aux2;
+    *aux = *pag;
+    aux->R = 1;
+    if(mem_p.size() < TAM_MEM_P/PAG_SIZE){
+        mem_p.push_back(aux);
+        proc->tabela_paginas[pag] = aux;
+    }else{
+        while(true){
+            if(mem_p[pont]->R == 0){
+                for(int i=0; i<processos.size(); i++){
+                    if(processos[i]->id == mem_p[pont]->process_id){
+                        for(map<pagina*,pagina*>::iterator it = processos[i]->tabela_paginas.begin(); it != processos[i]->tabela_paginas.end(); it++){
+                            if(it->second == mem_p[pont]){
+                                it->second = NULL;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                mem_p[pont] = aux;
+                proc->tabela_paginas[pag] = aux;
+                break;
+            }else{
+                mem_p[pont]->R = 0;
+                pont++;
+                pont = pont % TAM_MEM_P/PAG_SIZE;
+            }
+        }
+        pont++;
+        pont = pont % TAM_MEM_P/PAG_SIZE;
+    }
+}
+
 processo * cria_processo(int id, int tam, mem & mem_s){
     processo * x = new processo(id,tam);
     int num_pag = ceil(tam/PAG_SIZE);
     if(num_pag <= (TAM_MEM_S/PAG_SIZE - mem_s.paginas_mem.size())){
         for(int i=0; i< x->paginas.size(); i++){
-            x->paginas[i]->end = mem_s.prox_livre.top() * PAG_SIZE;
-            mem_s.prox_livre.pop();
             mem_s.paginas_mem.push_back(x->paginas[i]);
         }
     }
+    return x;
+}
+
+void * le(int iden, int end, vector<processo*> & processos, lista_circular & mem_p){
+    processo * aux;
+    pagina * aux2;
+    pagina * dado;
+    for(int i=0; i<processos.size(); i++){
+        if(processos[i]->id == iden){
+            aux = processos[i];
+        }
+    }
+    int pag = floor(end/PAG_SIZE);
+    int pos = end % PAG_SIZE;
+    
+    aux2 = aux->paginas[pag];
+    if(aux->tabela_paginas[aux2] == NULL){
+        mem_p.relogio(aux,aux2,processos);
+    }
+
+    dado = aux->tabela_paginas[aux2];
+
+    cout << dado->dados[pos] << endl;
+    
 }
 
 mem::mem(int tam)
 {
-    for(int i=tam/PAG_SIZE; i>=0; i--){
-        prox_livre.push(i);
-    }
+    mem::tam = tam;
 }
 
 mem::~mem()
@@ -94,9 +178,15 @@ mem::~mem()
 
 int main(){
     cout << " --- Simulador de gerenciamento de memória virtual com paginação --- \n\n";
-    mem mem_p(TAM_MEM_P);
+    lista_circular * mem_p = new lista_circular();
     mem mem_s(TAM_MEM_S);
-
-    map<pagina*,pagina*> teste;
+    vector<processo*> tabela_processos;
+    
+    for(int i=0; i<5;i++){
+        tabela_processos.push_back(cria_processo(i,500,mem_s));
+    }
+    for(int i=0; i<3;i++){
+        le(i,i,tabela_processos,*mem_p);
+    }
     
 }
