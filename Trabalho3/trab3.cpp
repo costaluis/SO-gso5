bd3a590cd84f8dd4987c8e77ee1515ec1c4f65ed
João Pedro Borges de Castro <42722284+jpbcastro@users.noreply.github.com>
2020-07-03 00:27:49 -0300

    mais uns comments

--- Trabalho3/trab3.cpp
+++ Trabalho3/trab3.cpp
@@ -23,9 +23,9 @@ public:
     char dados[PAG_SIZE];           // Conteudo da pagina
     int process_id;                 // ID do processo referente a pagina
     int R;                          // bit de controle para algoritmos de substituicao de páginas (LRU e relogio)
-    pagina();
-    pagina(int id);
-    ~pagina();
+    pagina();                       // declaração de um construtor para objetos da classe pagina (usado quando não é necessário atribuir valores logo na construção do objeto)
+    pagina(int id);                 // declaração de um construtor parametrizado para objetos da classe pagina (atribui valores logo na construção do objeto)
+    ~pagina();                      // declaração do destrutor para objetos da classe pagina
 };
 
 // Classe relativa a um processo
@@ -33,12 +33,12 @@ class processo
 {
 private:
 public:
-    int tam;                                // Tamanho em bytes do processo
-    int id;                                 // ID do processo
-    vector<pagina*> paginas;                // Vetor de paginas referente ao processo
-    map<pagina*,pagina*> tabela_paginas;    // Tabela de paginas referente ao processo
-    processo(int identificador, int tamanho);
-    ~processo();
+    int tam;                                    // Tamanho em bytes do processo
+    int id;                                     // ID do processo
+    vector<pagina*> paginas;                    // Vetor de paginas referente ao processo
+    map<pagina*,pagina*> tabela_paginas;        // Tabela de paginas referente ao processo
+    processo(int identificador, int tamanho);   // Declaração do construtor parametrizado
+    ~processo();                                // Declaração de destrutor
 };
 
 // Lista circular de paginas da memoria primaria
@@ -48,9 +48,9 @@ private:
 public:
     vector<pagina*> mem_p;          // Memoria primaria
     int pont;                       // Ponteiro que guarda o ID da proxima pagina
-    lista_circular();
-    ~lista_circular();
-    void carrega_mem(processo * proc, pagina * pag, vector<processo*> & processos,int flag);
+    lista_circular();               // Declaração do construtor
+    ~lista_circular();              // Declaração do destrutor
+    void carrega_mem(processo * proc, pagina * pag, vector<processo*> & processos,int flag);    // função que carrega uma pagina na memória primaria 
 };
 
 // Classe referente a memoria
@@ -60,11 +60,11 @@ private:
 public:
     vector<pagina*> paginas_mem;    // Vetor de paginas da memoria
     int tam;                        // Tamanho em bytes da memoria
-    mem(int tam);
-    ~mem();
+    mem(int tam);                   // Declaração de construtor parametrizado
+    ~mem();                         // Declaração de destrutor
 };
 
-// Construtor da classe pagina
+// Construtor parametrizado da classe pagina (atribui valores na construção)
 pagina::pagina(int id)
 {
     process_id = id;
@@ -74,15 +74,17 @@ pagina::pagina(int id)
     }
 }
 
+// Construtor da classe pagina
 pagina::pagina()
 {
 }
 
+// Destrutor da classe página
 pagina::~pagina()
 {
 }
 
-// Construtor da classe processo
+// Construtor parametrizado  da classe processo (atribui valores na construção)
 processo::processo(int identificador, int tamanho)
 {
     id = identificador;
@@ -93,15 +95,29 @@ processo::processo(int identificador, int tamanho)
     }
 }
 
+// Destrutor da classe processo
 processo::~processo()
 {
 }
 
+// Construtor da classe lista circular
 lista_circular::lista_circular()
 {
 }
 
+// Destrutor da classe lista circular
 lista_circular::~lista_circular()
+{
+}
+
+// Construtor parametrizado da classe mem (atribui, na construção, um valor ao tamanho da memória)
+mem::mem(int tam)
+{
+    mem::tam = tam;
+}
+
+// Destrutor da classe mem
+mem::~mem()
 {
 }
 
@@ -111,12 +127,12 @@ void lista_circular::carrega_mem(processo * proc, pagina * pag, vector<processo*
     int aux2;
     *aux = *pag;
     aux->R = 1;
-    if(mem_p.size() < TAM_MEM_P/PAG_SIZE){     // Verifica se a memoria primaria esta cheia
-        cout << "   Memória principal não está cheia." << endl;     // Se nao estiver, carrega a pagina na memoria primaria
+    if(mem_p.size() < TAM_MEM_P/PAG_SIZE){                              // Verifica se a memoria primaria esta cheia
+        cout << "   Memória principal não está cheia." << endl;         // Se nao estiver, carrega a pagina na memoria primaria
         mem_p.push_back(aux);
-        proc->tabela_paginas[pag] = aux;
+        proc->tabela_paginas[pag] = aux;                                // Coloca a nova pagina na tebela de paginas do processo
         cout << "   Página carregada na memória principal." << endl;
-    }else{      // Se estiver cheia, um dos algoritmos (indicado por flag) sera utilizado para substituir uma pagina
+    }else{                                                              // Se estiver cheia, um dos algoritmos (indicado por flag) sera utilizado para substituir uma pagina
         int i, j;
         map<pagina*,pagina*>::iterator it;
         cout << "   Memória principal cheia." << endl;
@@ -152,7 +168,7 @@ void lista_circular::carrega_mem(processo * proc, pagina * pag, vector<processo*
                 cout << "     > Página a ser substituída: página " << (j)*PAG_SIZE << "~" << (j+1)*PAG_SIZE -1 << " do Processo " << i+1 << "\n" << endl;
 
                 break;
-            }else{              // A pagina analisada nao eh a que sera substituida
+            }else{                      // A pagina analisada nao eh a que sera substituida
                 mem_p[pont]->R--;       // Decrementa o bit de controle do LRU/relogio
                 pont++;                 // Vai para a proxima pagina
                 pont = pont % (TAM_MEM_P/PAG_SIZE);     // Volta para o inicio da lista quando exceder o tamanho da memoria (lista circular)
@@ -182,8 +198,8 @@ processo * cria_processo(int id, int tam, mem & mem_s, vector<int> & proc_susp){
 
 // Funcao que faz verificacoes sobre um processo
 bool transf_ver(vector<processo*> processos, int & end, int proc, vector<int> & proc_susp){
-    for(int i=0; i<processos.size(); i++){      // Encontra o processo
-        if(processos[i]->paginas.size()*100 >= end){    // Verifica o espaco de enderecamento do processo
+    for(int i=0; i<processos.size(); i++){                  // Encontra o processo
+        if(processos[i]->paginas.size()*100 >= end){        // Verifica o espaco de enderecamento do processo
             if(processos[i]->id == proc){
                 for(int j=0; j<proc_susp.size(); j++){      // Verifica se o processo esta suspenso
                     if(proc_susp[j]==proc){
@@ -242,19 +258,20 @@ void le(int iden, int end, vector<processo*> & processos, lista_circular & mem_p
     
 }
 
+// Função para realizar escrita
 void escreve(int iden, int end, vector<processo*> & processos, lista_circular & mem_p, int flag,vector<int> & proc_susp){
     processo * aux;
     pagina * aux2;
     pagina * dado;
-    if(!transf_ver(processos,end,iden,proc_susp)){
-        return;
+    if(!transf_ver(processos,end,iden,proc_susp)){  //verifica se o endereço pertence ao processo e se este não está suspenso
+        return;                                     //caso algumas dessas condições não seja satisfeita, a função finaliza
     }
-    for(int i=0; i<processos.size(); i++){
-        if(processos[i]->id == iden){
+    for(int i=0; i<processos.size(); i++){          //percorre o vetor de processos e encontra o processo que possui a id "iden"
+        if(processos[i]->id == iden){               //este processo será passado por referencia à variável aux
             aux = processos[i];
         }
     }
-    int pag = floor(end/PAG_SIZE);
+    int pag = floor(end/PAG_SIZE);                  
     int pos = end % PAG_SIZE;
     
     aux2 = aux->paginas[pag];
@@ -276,109 +293,102 @@ void escreve(int iden, int end, vector<processo*> & processos, lista_circular & 
     cout << "   Atualização da memória secundária realizada." << endl;
 }
 
+// Função para instrução a ser executada pela CPU
 void inst_CPU(vector<int> proc_susp, vector<processo*> processos, int id, int inst){
     bool flag = false;
-    for(int i=0; i<proc_susp.size(); i++){
-        if(proc_susp[i]==id){
+    for(int i=0; i<proc_susp.size(); i++){                              // percorre o vetor de processos suspensos, verificando, através da id, se o processo que deve realizar a instrução está entre eles
+        if(proc_susp[i]==id){                                           // caso esteja, a função informa que o processo está suspenso e termina sua execução
             cout << "   Processo " << id << " esta suspenso." << endl;
             return;
         }
     }
-    for(int i=0; i<processos.size(); i++){
-        if(processos[i]->id == id){
+    for(int i=0; i<processos.size(); i++){                              // percorre o vetor de processos criados, verificando, através da id, se o processo que deve realizar a instrução está entre eles
+        if(processos[i]->id == id){                                     // caso esteja, sinaliza uma flag
             flag = true;
         }
     }
-    if(!flag){
+    if(!flag){                                                          // caso a flag não tenha sido sinalizada a função informa que o processo não existe e termina sua execução
         cout << "   Processo " << id << " nao existe." << endl;
         return;
     }
-
-    cout << "   Processo " << id << " executou a instrucao " << inst << " pela CPU." << endl;
+    cout << "   Processo " << id << " executou a instrucao " << inst << " pela CPU." << endl;       //caso a função tenha chegado a esse ponto (significando que o processo está no vetor de processos criados) a função imprie que ele realizou a execução pela CPU
 }
 
+// Função para executar instrução de entrada e saída
 void inst_IO(vector<int> proc_susp, vector<processo*> processos, int id, int inst){
     bool flag = false;
-    for(int i=0; i<proc_susp.size(); i++){
-        if(proc_susp[i]==id){
+    for(int i=0; i<proc_susp.size(); i++){                              // percorre o vetor de processos suspensos, verificando, através da id, se o processo que deve realizar a instrução está entre eles
+        if(proc_susp[i]==id){                                           // caso esteja, a função informa que o processo está suspenso e termina sua execução
             cout << "   Processo " << id << " esta suspenso." << endl;
             return;
         }
     }
-    for(int i=0; i<processos.size(); i++){
-        if(processos[i]->id == id){
+    for(int i=0; i<processos.size(); i++){                              // percorre o vetor de processos criados, verificando, através da id, se o processo que deve realizar a instrução está entre eles
+        if(processos[i]->id == id){                                     // caso esteja, sinaliza uma flag
             flag = true;
         }
     }
-    if(!flag){
+    if(!flag){                                                          // caso a flag não tenha sido sinalizada a função informa que o processo não existe e termina sua execução
         cout << "   Processo " << id << " nao existe." << endl;
         return;
     }
-    cout << "   Processo " << id << " executou a instrucao " << inst << " de IO." << endl;
+    cout << "   Processo " << id << " executou a instrucao " << inst << " de IO." << endl;      //caso a função tenha chegado a esse ponto (significando que o processo está no vetor de processos criados) a função imprie que ele realizou a instrução de IO
 }
 
-mem::mem(int tam)
-{
-    mem::tam = tam;
-}
-
-mem::~mem()
-{
-}
-
+// Função para leitura do arquivo de entrada
 int le_arq(FILE * arq, int& proc, char & inst, int & tam){
-    char aux[5];
+    char aux[5];                                            // string armazenará o processo, exemplo: P7
 
-    if(fscanf(arq,"%s %c %d", aux, &inst, &tam) == EOF){
+    if(fscanf(arq,"%s %c %d", aux, &inst, &tam) == EOF){    // lê uma linha do arquivo, caso seja o final do arquivo, retorna 1
         return 1;
     }
-    proc = atoi(aux+1);
+    proc = atoi(aux+1);                                     // carregará o numero do processo, em int, na variavel proc 
     return 0;
 }
 
 int main(){
     cout << " --- Simulador de gerenciamento de memória virtual com paginação --- \n\n";
-    lista_circular * mem_p = new lista_circular();
-    mem mem_s(TAM_MEM_S);
-    vector<processo*> tabela_processos;
-    vector<int> processos_suspensos;
-    FILE * arq;
+    lista_circular * mem_p = new lista_circular();          //cria um objeto mem_p (memória principal) do tipo lista_circular
+    mem mem_s(TAM_MEM_S);                                   //cria um objeto mem_s (memória secundária) do tipo mem
+    vector<processo*> tabela_processos;                     //declara o vetor tabela_processos que armazenará ponteiros para os endereços de memória virtual dos processos criados
+    vector<int> processos_suspensos;                        //declara o vetor de inteiros "processos_suspensos", que armazenará o id dos processos suspensos 
+    FILE * arq;                                             // O arquivo de texto "entrada.txt" é aberto para leitura
     arq = fopen("entrada.txt","rt");
-    if(arq == NULL){
+    if(arq == NULL){                                        //Se não existir o arquivo, é impresso na tela "Arquivo não encontrado"
         cout << "Arquivo nao encontrado" << endl;
         return 0;
     }
-    
-    char inst;
-    int tam, proc;
-    while(!le_arq(arq,proc,inst,tam)){
-        cout << "--> Processo: " << proc << " | " << " Instrucao: " << inst << " | " << " Endereço/Tamanho: " << tam << endl;
+    char inst;                                              // "inst" armazenará a instrução do processo
+    int tam, proc;                                          // "proc" armazenará o número identificador do processo e "tam" o tamanho ou o endereço de memória que o processo deverá utilizar
+    while(!le_arq(arq,proc,inst,tam)){                      //executará enquanto for 0, ou seja, até achar o final do arquivo
+        cout << "--> Processo: " << proc << " | " << " Instrucao: " << inst << " | " << " Endereço/Tamanho: " << tam << endl;           // imprime a as instruções que serão executadas
         switch (inst)
         {
-        case 'C':
+        case 'C':           // caso a instrução seja de criação do processo
             tabela_processos.push_back(cria_processo(proc,tam,mem_s,processos_suspensos));
             getc(stdin);
             break;
-        case 'R':
+        case 'R':           // caso a instrução seja de leitura
             le(proc,tam,tabela_processos,*mem_p,1,processos_suspensos);
             getc(stdin);
             break;
-        case 'W':
+        case 'W':           // caso a instrução seja de escrita
             escreve(proc,tam,tabela_processos,*mem_p,1,processos_suspensos);
             getc(stdin);
             break;
-        case 'I':
+        case 'I':           // caso a instrução seja de IO
             inst_IO(processos_suspensos,tabela_processos,proc,tam);
+            getc(stdin);
             break;
-        case 'P':
+        case 'P':           // caso a instrução seja execução pela CPU
             inst_CPU(processos_suspensos,tabela_processos,proc,tam);
+            getc(stdin);
             break;
-        default:
+        default:            // caso a instrução seja diferente das definidas
             cout << "Instrucao invalida!" << endl;
+            getc(stdin);
             break;
         }
     }
-    
-
     
 }
